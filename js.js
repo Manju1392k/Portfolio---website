6a4d18739b4fb3a8f97fbc1c047157276d790da3
Manjunath <sreerammanjunath1392@gmail.com>
2022-06-23 11:28:31 +0530

    PortfolioWebsite

---
+++ js.js
@@ -0,0 +1,9 @@
+function OpenMenu() {
+  let Screen = document.getElementById("openmenu");
+  Screen.style.width = "100vw";
+}
+
+function Close() {
+  let Screen = document.getElementById("openmenu");
+  Screen.style.width = "0vw";
+}
